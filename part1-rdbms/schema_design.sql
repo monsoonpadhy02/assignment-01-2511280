f2b4a8194693a389368464e7a7b6b5ba731f4e75
monsoonpadhy02 <monsoonpadhy02@gmail.com>
2026-03-23 23:37:50 +0530

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,34 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    email VARCHAR(100)
+);
 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR(10),
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE order_items (
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+INSERT INTO customers VALUES
+('CUST001','Rohan Iyer','Kolkata','user1@example.com'),
+('CUST002','Aditya Shah','Mumbai','user2@example.com'),
+('CUST003','Arjun Singh','Pune','user3@example.com'),
+('CUST004','Neha Joshi','Mumbai','user4@example.com'),
+('CUST005','Divya Joshi','Mumbai','user5@example.com');
